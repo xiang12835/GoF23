cb49179c0533595c41afc8533bbb007095a559a6
Shane Yoo <yu.xiang@alibaba-inc.com>
2021-12-29 22:40:00 +0800

    upd_singleton

---
+++ PatternJava/src/main/java/online/flyingfish/pattern/Singleton/Main.java
@@ -0,0 +1,13 @@
+package online.flyingfish.pattern.Singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Singleton1 s1 = Singleton1.getInstance();
+        Singleton2 s2 = Singleton2.getInstance();
+        Singleton3 s3 = Singleton3.getInstance();
+        Singleton4 s4 = Singleton4.getInstance();
+        Singleton5 s5 = Singleton5.getInstance();
+    }
+
+}
